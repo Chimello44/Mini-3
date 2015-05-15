1979d30f3656ba018f4de67a3a64a86aad6c03f9
vitorsala <vitorsala@gmail.com>
2015-05-15 16:37:12 -0300

    Leeeroyyyyy Jenkins! - Feito um protótipo de telas via storyboard e xib - Views views e views..... vendo views! - Agora só falta os controllers  :x

---
+++ Mini#3/Mini#3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mini#3
+//
+//  Created by Vitor Kawai Sala on 14/05/15.
+//  Copyright (c) 2015 Los caras com escritório legal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
