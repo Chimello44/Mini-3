7f088370b1e51499ce581a17893e86187cc447a7
Samuel Shin Kim <Samuel@Samuels-Macbook-Pro.local>
2015-05-15 16:53:53 -0300

    Grupos e classes criados

---
+++ Mini#3/Mini#3/Group.swift
@@ -0,0 +1,9 @@
+//
+//  Group.swift
+//  Mini#3
+//
+//  Created by Samuel Shin Kim on 15/05/15.
+//  Copyright (c) 2015 Los caras com escritório legal. All rights reserved.
+//
+
+import Foundation
---
+++ Mini#3/Mini#3/VoiceReq.swift
@@ -0,0 +1,13 @@
+//
+//  VoiceReq.swift
+//  Mini#3
+//
+//  Created by Samuel Shin Kim on 15/05/15.
+//  Copyright (c) 2015 Los caras com escritório legal. All rights reserved.
+//
+
+import Foundation
+
+class VoiceReq: Media {
+    
+}
